7c56658ecbca90893c2dc45ce1b7ff7eebee746c
Karru <shivakumar.karru@thermofisher.com>
2024-12-07 02:16:28 +0530

    first_Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [state, setName] = useState({
+    greet : "",
+    name : ""
+  })
+  const onChange = (e)=>{
+    const {value} = e.target
+    setName({...state,greet:"Hello",name:value+"!"})
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>Enter your name:</h2>
+        <input type="text" name="input" id="input" onChange={onChange}></input>
+        <h2>{state.greet} {state.name}</h2>
     </div>
   )
 }
